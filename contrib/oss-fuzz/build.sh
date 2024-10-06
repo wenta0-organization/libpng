c02ca3cb9e11da247b25f811512708273b5efc8b
Tian Qiu <qiutianchloe@hotmail.com>
2024-10-06 17:21:29 +1100

    update the build.sh to run local public corpus download 27/09/2024

--- contrib/oss-fuzz/build.sh
+++ contrib/oss-fuzz/build.sh
@@ -43,8 +43,11 @@ $CXX $CXXFLAGS -std=c++11 -I. \
      -lFuzzingEngine .libs/libpng16.a -lz
 
 # add seed corpus.
-find $SRC/libpng -name "*.png" | grep -v crashers | \
-     xargs zip $OUT/libpng_read_fuzzer_seed_corpus.zip
+# find $SRC/libpng -name "*.png" | grep -v crashers | \
+#      xargs zip $OUT/libpng_read_fuzzer_seed_corpus.zip
+
+zip -j $OUT/libpng_read_fuzzer_seed_corpus.zip $SRC/libpng/contrib/oss-fuzz/libpng_read_fuzzer/*
+
 
 cp $SRC/libpng/contrib/oss-fuzz/*.dict \
      $SRC/libpng/contrib/oss-fuzz/*.options $OUT/
